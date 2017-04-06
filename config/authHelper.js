53198e7d515e79ce000437b643eb96bc46fcd3e3
Jessica Prado <jessica.marie.prado@gmail.com>
2017-04-06 16:51:48 -0400

    bad request

--- config/authHelper.js
+++ config/authHelper.js
@@ -11,10 +11,11 @@ var credentials = {
 };
 var oauth2 = require('simple-oauth2').create(credentials);
 
-var redirectUri = 'http://localhost:3000/authorize';
+var redirectUri = 'http://localhost:8000/authorize';
 
 // The scopes the app requires
 var scopes = [ 'openid',
+               'offline_access',
                'https://outlook.office.com/mail.read' ];
     
 function getAuthUrl() {
@@ -25,5 +26,32 @@ function getAuthUrl() {
   console.log('Generated auth url: ' + returnVal);
   return returnVal;
 }
+
+function getTokenFromCode(auth_code, callback, response) {
+  var token;
+  oauth2.authorizationCode.getToken({
+    code: auth_code,
+    redirect_uri: redirectUri,
+    scope: scopes.join(' ')
+  }, function (error, result) {
+    if (error) {
+      console.log('Access token error: ', error.message);
+      callback(response, error, null);
+    } else {
+      token = oauth2.accessToken.create(result);
+      console.log('Token created: ', token.token);
+      callback(response, null, token);
+    }
+  });
+};
+
+function refreshAccessToken(refreshToken, callback) {
+  var tokenObj = oauth2.accessToken.create({refresh_token: refreshToken});
+  tokenObj.refresh(callback);
+};
+
+exports.refreshAccessToken = refreshAccessToken;
+
+exports.getTokenFromCode = getTokenFromCode;
 
 exports.getAuthUrl = getAuthUrl;
