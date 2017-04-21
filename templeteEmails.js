29b3bf5c6a6b4e86a2ee9f3791fd935352b0db74
Jessica Prado <jessica.marie.prado@gmail.com>
2017-04-21 16:34:51 -0400

    template email check

---
+++ templeteEmails.js
@@ -0,0 +1,9 @@
+module.exports = {
+	bridal: [
+		{
+			subject: 'Assist Needed for Bridal: ',
+            body: 'Team, We need an assist for a Bridal',
+            signature: 'Jessica',
+		}
+	]
+}
