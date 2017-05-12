d064babbf7045220b9a40b58625b5675e7b3a4db
Jessica Prado <jessica.marie.prado@gmail.com>
2017-05-12 16:06:35 -0400

    server notes

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ var bodyParser = require('body-parser');
 PORT = process.env.PORT || 8000;
 
 app.use(express.static(__dirname + '/app/api/outlook'));
-
+//body parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.text({ type: 'text/html' }));
 
+//required js for routes
 var routes = require('./outlook/routes');
 app.use('/', routes);
 
