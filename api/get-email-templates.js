9a9a7dcadc0ce62a1e733e17649ecec15993c12f
Jessica Prado <jessica.marie.prado@gmail.com>
2017-06-11 09:40:33 -0400

    added bower.json

--- api/get-email-templates.js
+++
@@ -1,7 +0,0 @@
-var template = require('../templeteEmails');
-
-module.exports = function(app) {
-	app.get('/templateEmails', function(req, res) {
-		res.send(template.bridal);
-	});
-}
--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var app = express();
-var path = require('path'); 
+var morgan = require('morgan');
+var path = require('path');
 
 // Initialize variables. 
-var PORT = process.env.PORT || 8000; 
+var port = process.env.PORT || 8080;
 
+// Configure morgan module to log all requests.
+app.use(morgan('dev'));
+
+// Set the front-end folder to serve public assets.
+app.use(express.static(__dirname + '/public'));
+app.use("/bower_components", express.static(path.join(__dirname, 'bower_components')));
+
+// Set up our one route to the index.html file.
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/schedulingEmails/index.html'));
+});
 
 // Start the server.  
-app.listen(PORT, function() {
-	console.log("Now creeping on " + PORT)
-});
+app.listen(port);
+console.log('Listening on port ' + port + '...'); 
