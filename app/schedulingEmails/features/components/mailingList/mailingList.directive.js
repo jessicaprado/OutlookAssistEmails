055d5c76465c61239eaaa0c3047f14c0b83b81e1
Jessica Prado <jessica.marie.prado@gmail.com>
2017-06-13 18:14:26 -0400

    notes for future

--- app/schedulingEmails/features/components/mailingList/mailingList.directive.js
+++ app/schedulingEmails/features/components/mailingList/mailingList.directive.js
@@ -21,7 +21,9 @@ function MailingCtrl($scope) {
     vm.mailList = '';
 
     $scope.$watch('MailingVM.mailList', function(value) {
-        console.log(value);
+        vm.mailList = value;
+        console.log(vm.mailList);
     });
-
+//this sets the value in the watch function, but does not actually set it.
+// Make sure when the send button is hit, you puch this value to it's variable and make the correct http call
 }
