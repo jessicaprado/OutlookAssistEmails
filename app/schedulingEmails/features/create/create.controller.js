f368c1fed2da9f7f899f5319c61ed874f28a2f87
Jessica Prado <jessica.marie.prado@gmail.com>
2017-06-16 13:20:09 -0400

    create ctrl updated

--- app/schedulingEmails/features/create/create.controller.js
+++ app/schedulingEmails/features/create/create.controller.js
@@ -13,40 +13,4 @@ function CreateCtrl(contactFolders) {
 
 }
 
-
-  //   POST https://graph.microsoft.com/v1.0/me/sendMail
-  //       Content-type: application/json
-  //   Content-length: 512
-  //
-  //   {
-  //       "message": {
-  //       "from": {"@odata.type": "dfap.scheduler@disney.com"},
-  //       "sender": {"@odata.type": "microsoft.graph.recipient"},
-  //       "subject": "Assist Needed for",
-  //           "body": {
-  //           "contentType": "Text",
-  //               "content": "The new cafeteria is open."
-  //       },
-  //       "toRecipients": [
-  //           {
-  //               "emailAddress": {
-  //                   "address": "jessica.marie.prado@gmail.com"
-  //               }
-  //           }
-  //       ],
-  //           "ccRecipients": [
-  //           {
-  //               "emailAddress": {
-  //                   "address": "dfap.scheduler@disney.com"
-  //               }
-  //           }
-  //       ]
-  //   },
-  //       "saveToSentItems": "true"
-  //   }
-  //
-  //
-  // };
-
-
 })(); //end of angular function
